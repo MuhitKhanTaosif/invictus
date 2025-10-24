bc8914478a354b74afc14a18b56a7da26a4e5776
muhit_dev <muhit@clickbit.com.au>
2025-10-24 09:20:52 +0000

    layout and env updated

--- client/src/Layout/AdminView.jsx
+++ client/src/Layout/AdminView.jsx
@@ -6,13 +6,9 @@ import Sidebar from "../components/Sidebar";
 //This layout is only for admin
 export default function AdminView() {
     return (
-        <div className="flex h-screen bg-gray-50 dark:bg-gray-900">
+        <div>
             <Sidebar />
-            <main className="flex-1 ml-64 overflow-y-auto">
-                <div className="min-h-full">
-                    <Outlet />
-                </div>
-            </main>
+            <Outlet />
         </div>
     );
 }
